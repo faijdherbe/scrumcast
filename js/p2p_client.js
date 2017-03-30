d73a56375eef5083d94b32e014a20f8f2601fc04
Bob Vork <bob@codingdutchmen.com>
2017-03-30 16:12:21 +0200

    Aantal minor styling improvements

--- js/p2p_client.js
+++ js/p2p_client.js
@@ -2,7 +2,7 @@ var peer = new Peer({key: 'jmikr170cgv0wwmi'});
 
 peer.on('open', function(id) {
 	console.log('My peer ID is: ' + id);
-	
+
 });
 
 var conn = peer.connect('TOO');
@@ -20,15 +20,17 @@ conn.on('open', function(conn) {
 	$('#profile').removeClass("hidden");
 })
 
-function sendPoints(score) {
+function sendPoints(score, el) {
 	conn.send({
 		score: score
 	});
+    $('.card').removeClass("selected");
+    $(el).addClass("selected");
 }
 var avatar_id = 1;
 function selectAvatar(id) {
-	$(".avatar").removeClass("selected");
-	$(".avatar_"+id).addClass("selected");
+    $(".avatar").removeClass("selected");
+    $(".avatar_"+id).addClass("selected");
 	avatar_id = id;
 }
 
