100100512240b75f0fb529436fec6401290528d1
Jeroen Faijdherbe <jeroen@theothersonline.eu>
2018-07-05 16:05:02 +0200

    hardcoded key

--- js/p2p.js
+++ js/p2p.js
@@ -5,7 +5,7 @@ var c = undefined;
 function startCanvas(canvasID, peerJsKey) {
 
 	var peer = new Peer('TOO', {
-		key: peerJsKey,
+		key: 'jmikr170cgv0wwmi',
 		host: '192.168.10.19', port: 9000
 	});
 
